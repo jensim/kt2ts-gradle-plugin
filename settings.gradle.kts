a69e4ba22a648d8fc3f62b35c3939e74a7d0441e
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-14 08:39:05 +0200

    Make settings in kotlin DSL as well

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+/*pluginManagement {
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+        jcenter()
+        mavenLocal()
+    }
+}*/
+
+rootProject.name = "kt2ts-plugin"
