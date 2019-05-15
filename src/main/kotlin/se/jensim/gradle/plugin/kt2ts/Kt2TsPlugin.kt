d7bdda2687b2fd425dc5aeb15b8ad1d8bf5ba32c
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-15 10:49:59 +0200

    Print information about task dependency if we are unable to find dependencies or dependents

--- src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPlugin.kt
+++ src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPlugin.kt
@@ -3,17 +3,23 @@ package se.jensim.gradle.plugin.kt2ts
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-
 open class Kt2TsPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.extensions.create("kt2ts", Kt2TsPluginExtension::class.java)
         val task = target.tasks.create("kt2ts", Kt2TsTask::class.java)
-        target.tasks.findByName("processResources")?.dependsOn(task)
-        task.dependsOn(
-            listOfNotNull(
-                target.tasks.findByName("compileKotlin"),
-                target.tasks.findByName("compileJava")
-            )
-        )
+
+        target.tasks.findByName("processResources")
+            ?.dependsOn(task)
+            ?: System.err.println("Could not find the processResources task. You might need to set up task order manually.")
+
+        val dependencies = listOfNotNull(
+            target.tasks.find { it.name.startsWith("compile") }
+        ).filterNot { it.name.startsWith("compileTest") }
+
+        if (dependencies.isEmpty()) {
+            System.err.println("Could not find any compile tasks. You might need to set up task order manually.")
+        } else {
+            task.dependsOn(dependencies)
+        }
     }
 }
