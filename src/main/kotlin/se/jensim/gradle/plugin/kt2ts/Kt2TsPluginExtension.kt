66a80233f4108b9f3b9da66358f98a57c3e1b8f4
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-19 07:52:13 +0200

    Use taskprovider

--- src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPluginExtension.kt
+++ src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPluginExtension.kt
@@ -3,6 +3,7 @@ package se.jensim.gradle.plugin.kt2ts
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.TaskContainer
+import org.gradle.api.tasks.TaskProvider
 import java.io.File
 
 open class Kt2TsPluginExtension {
@@ -16,4 +17,4 @@ fun Project.kt2ts(config: Kt2TsPluginExtension.() -> Unit) {
     extensions.configure("kt2ts", config)
 }
 
-val TaskContainer.kt2ts: Kt2TsTask get() = findByName("kt2ts") as Kt2TsTask
+val TaskContainer.kt2ts: TaskProvider<Kt2TsTask> get() = named("kt2ts", Kt2TsTask::class.java)
