787d09fcf62ed07098d997d6373f874e79ea1998
Jens Brimfors <jens.brimfors@nordnet.se>
2019-05-19 07:44:30 +0200

    Added extension getter for project.tasks.kt2ts

--- src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPluginExtension.kt
+++ src/main/kotlin/se/jensim/gradle/plugin/kt2ts/Kt2TsPluginExtension.kt
@@ -2,6 +2,7 @@ package se.jensim.gradle.plugin.kt2ts
 
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.TaskContainer
 import java.io.File
 
 open class Kt2TsPluginExtension {
@@ -10,6 +11,8 @@ open class Kt2TsPluginExtension {
     var outputFile: File? = null
 }
 
-fun Project.`kt2ts`(config: Kt2TsPluginExtension.() -> Unit) {
+fun Project.kt2ts(config: Kt2TsPluginExtension.() -> Unit) {
     extensions.configure("kt2ts", config)
 }
+
+val TaskContainer.kt2ts: Kt2TsTask get() = findByName("kt2ts") as Kt2TsTask
